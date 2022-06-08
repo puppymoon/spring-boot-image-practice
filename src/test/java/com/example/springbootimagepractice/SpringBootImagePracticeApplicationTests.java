c15e5c79e4028afc39f2dd4a6c78e12bcb500c78
黃越 <moontea306@gmail.com>
2022-06-08 22:15:28 +0800

    init

---
+++ src/test/java/com/example/springbootimagepractice/SpringBootImagePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootimagepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootImagePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
